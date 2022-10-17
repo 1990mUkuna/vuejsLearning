57525ab4011c4db546c44a1ea59a930642526ac0
Mukuna90 <mukuna.mukuna@thedelta.io>
2022-10-17 11:45:51 +0200

    WIP

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-/* const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+Vue.createApp({
+    data(){
+        return {
+           goals: [],
+           enteredValue: ''
+        };
+    },
 
-function addGoal(){
-    const enteredValue = inputEl.value;
-    const listItemEl = document.createElement('li');
-    listItemEl.textContent = enteredValue;
-    listEl.appendChild(listItemEl);
-}
-buttonEl.addEventListener('click', addGoal); */
+    methods:{
+        addGoal(){
+            this.goals.push(this.enteredValue)
+        }
+    }
+})
